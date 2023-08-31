2d5b8f64efe21199e0ffba7cab8fa3804e22a2dd
John Michael <johnny25580@gmail.com>
2023-08-31 03:31:59 +0100

    add CATEGORY FEATURE

--- src/main/java/com/bestbuy/ecommerce/service/CategoryService.java
+++ src/main/java/com/bestbuy/ecommerce/service/CategoryService.java
@@ -1,7 +1,9 @@
 package com.bestbuy.ecommerce.service;
 
+import com.bestbuy.ecommerce.domain.entity.Category;
 import com.bestbuy.ecommerce.dto.request.CategoryRequest;
 import com.bestbuy.ecommerce.dto.responses.CategoryResponse;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -17,4 +19,6 @@ public interface CategoryService {
     CategoryResponse editCategory(Long categoryId, CategoryRequest categoryRequest);
 
     String  deleteCategoryById(Long categoryId);
+
+    Page<Category> filterAndSearchCategory(int pageNo, int pageSize, String sortBy, String firstName);
 }
