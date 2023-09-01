90879fee6a583e1d6aa8e751b704daa489cc5a90
John Michael <johnny25580@gmail.com>
2023-09-01 03:58:13 +0100

    CategorySearchPage.

--- src/main/java/com/bestbuy/ecommerce/search/CategorySearchPage.java
+++ src/main/java/com/bestbuy/ecommerce/search/CategorySearchPage.java
@@ -1,4 +1,15 @@
 package com.bestbuy.ecommerce.search;
 
+import lombok.Data;
+import org.springframework.data.domain.Sort;
+@Data
+
 public class CategorySearchPage {
+    private int pageNo = 0;
+
+    private  int pageSize = 5;
+
+    private String sortByName = "name";
+
+    private Sort.Direction sortDirection = Sort.Direction.ASC;
 }
